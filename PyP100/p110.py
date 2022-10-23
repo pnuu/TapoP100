9e2a0c1e146911abea982cc48b28e057695577fa
Panu Lahtinen <pnuu+git@iki.fi>
2022-10-23 16:46:24 +0300

    Refactor P110 class

--- PyP100/p110.py
+++ PyP100/p110.py
@@ -14,21 +14,21 @@ class P110(TapoBase):
 
     def get_energy_usage(self):
         """Get the energy usage from the device."""
-        url = f"http://{self.ip_address}/app?token={self._token}"
-        payload = {
-            "method": "get_energy_usage",
-            "requestTimeMils": int(round(time.time() * 1000)),
-        }
-        payload = {
-            "method": "securePassthrough",
-            "params": {
-                "request": self._tplink_cipher.encrypt(json.dumps(payload))
-            }
-        }
+        payload = self._get_secure_payload(
+            _get_energy_usage_payload()
+        )
         LOGGER.debug("getEnergyUsage %s", self.ip_address)
 
-        response = self._get_response(url, payload)
-
-        self._log_errors(response)
+        try:
+            response = self._get_response(f"http://{self.ip_address}/app?token={self._token}", payload)
+        finally:
+            self._log_errors(response)
 
         return json.loads(response)
+
+
+def _get_energy_usage_payload():
+    return {
+        "method": "get_energy_usage",
+        "requestTimeMils": int(round(time.time() * 1000)),
+    }
